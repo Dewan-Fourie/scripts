4855a7fc6902139c6841555dbc5df1e904b2e4fa
Dewan-Fourie <dewan@tiamat.com.au>
2022-07-29 15:55:11 +0930

    Finalised template and ran test

--- Get Users With Send As Permissions.ps1
+++ Get Users With Send As Permissions.ps1
@@ -1,9 +1,11 @@
 # Use (Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5) in powershell to install nessesary EXO V2 modules
-# https://www.michev.info/Blog/Post/2997/connecting-to-exchange-online-powershell-via-client-secret-flow <- 
+# https://www.michev.info/Blog/Post/2997/connecting-to-exchange-online-powershell-via-client-secret-flow <- Used as reference
 
-Connect-ExchangeOnline -UserPrincipalName 'dewan.nfit@happyhaven.sa.edu.au'
+Import-Module "<location of EXO V2 (ExchangeOnlineManagement) modules>"
+
+Connect-ExchangeOnline -UserPrincipalName '<global admin email>'
 
 # Enter mailbox to check below
-Get-Mailbox 'accounts@happyhaven.sa.edu.au' | Select-Object Name,Alias,UserPrincipalName,PrimarySmtpAddress,@{l='SendOnBehalfOf';e={$_.GrantSendOnBehalfTo -join ";"}} | Export-CSV "C:\SendOnBehalf.csv"
+Get-Mailbox '<enter mailbox>' | Select-Object Name,Alias,UserPrincipalName,PrimarySmtpAddress,@{l='SendOnBehalfOf';e={$_.GrantSendOnBehalfTo -join " ;"}} | Export-CSV "C:\temp\SendOnBehalf.csv"
 
 Disconnect-ExchangeOnline
