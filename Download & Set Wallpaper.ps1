57c87419bdfb3c15d4aa1a0ee84ab17c339b6de3
Dewan-Fourie <dewan@tiamat.com.au>
2022-08-26 11:49:40 +0930

    Completed and Tested

--- Download & Set Wallpaper.ps1
+++ Download & Set Wallpaper.ps1
@@ -7,7 +7,7 @@ Function Set-WallPaper($Value)
 New-Item -Path "c:\" -Name "wp" -ItemType "directory" -Force
 
 # Enter the wallpaper URL
-$url = "https://prd-sc101-cdn.rtx.com/-/media/rmd/news/2019/12/20/arming-the-f-35/images/f-35a-fighters.jpg?rev=56ddf7086b49435b900fb4d7b227cc19"
+$url = "http://manage.leal.com.au/HappyHaven/Background/background.jpg"
 
 if ((Invoke-WebRequest -Uri $url -Method 'POST' -OutFile C:\wp\wp.jpg).StatusCode -eq 200) {
     Invoke-WebRequest -Uri $url -OutFile C:\wp\wp.jpg
