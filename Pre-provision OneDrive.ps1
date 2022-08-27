ee059dd27795f3ea42725223f167fd7f4daab1e2
Dewan-Fourie <dewan@tiamat.com.au>
2022-08-27 12:05:58 +0930

    Completed and Tested

--- Pre-provision OneDrive.ps1
+++ Pre-provision OneDrive.ps1
@@ -1,12 +1,19 @@
-# Use (Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5) in powershell to install nessesary EXO V2 modules
-# https://www.michev.info/Blog/Post/2997/connecting-to-exchange-online-powershell-via-client-secret-flow <- Used as reference
+# Use (Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope Allusers) to install Microsoft Online SharePoint PowerShell modules.
+# Use (Get-Module -listavailable) to find the path of 'Microsoft.Online.SharePoint.PowerShell' and enter below.
+Import-Module "C:\Program Files\PowerShell\Modules\Microsoft.Online.SharePoint.PowerShell" # This may be different to your environment.
+$credential = Get-credential
+Connect-SPOService -url 'https://<tenant>-admin.sharepoint.com/' -Credential $credential #Use the global admin credentials.
 
-Import-Module "<location of EXO V2 (ExchangeOnlineManagement) modules>"
+$InputFilePath = "<Full path of csv file>" #Set this to the CSV file of the accounts you want to provision.
+# The csv file needs 'User' as the header, with all the accounts listed under it.
 
-Connect-ExchangeOnline -UserPrincipalName '<global admin email>'
+$CsvFile = Import-Csv $InputFilePath
 
-# Enter mailbox to check below
-$users = Get-Content -path "<Path of the text file containing newline separated users>"
-Request-SPOPersonalSite -UserEmails $users
+ForEach ($line in $CsvFile)
+{ 
+    Request-SPOPersonalSite -UserEmails $line.User -NoWait
+    Write-Host Personal site provisioned for $line.User -ForegroundColor Yellow
+}
 
-Disconnect-ExchangeOnline
+# If facing issues, use (Uninstall-Module -Name Microsoft.Online.SharePoint.PowerShell) to uninstall modules, and re-install.
+# It might be necessary to manually delete all folders starting with 'Microsoft.SharePoint' in 'C:\Windows\Microsoft.NET\assembly\GAC_MSIL'
