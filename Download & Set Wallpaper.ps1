040b09e615f82ee4adf13f608f624ea1fdfba4c1
Dewan-Fourie <dewan@tiamat.com.au>
2022-09-02 15:48:47 +0930

    Removed personal info

--- Download & Set Wallpaper.ps1
+++ Download & Set Wallpaper.ps1
@@ -2,19 +2,7 @@
 # as the script does not have the same licensing limitations 
 # as the "Desktop Background" configuration profile.
 
-Function Set-WallPaper($Value)
-{
- Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
- rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True
-}
-
 New-Item -Path "c:\" -Name "wp" -ItemType "directory" -Force
-
-# Enter the wallpaper URL
-$url = "https://www.leal.com.au/wallpapers/happyhaven/background.jpg"
-
-if ((Invoke-WebRequest -Uri $url -Method 'POST' -OutFile C:\wp\background.jpg).StatusCode -eq 200) {
-    Invoke-WebRequest -Uri $url -OutFile C:\wp\background.jpg
-}
-
-Set-WallPaper -value "C:\wp\background.jpg"
+Invoke-WebRequest -Uri "https://www.leal.com.au/wallpapers/happyhaven/background.jpg" -OutFile C:\wp\background.jpg
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\wp\background.jpg"
+rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True
