cd716d0be20fd0b2fd58944e4c605ca0b4610d2c
Dewan-Fourie <dewan@tiamat.com.au>
2022-09-12 08:50:37 +0930

    Changed script to use variables

--- Download & Set Wallpaper.ps1
+++ Download & Set Wallpaper.ps1
@@ -2,7 +2,29 @@
 # as the script does not have the same licensing limitations 
 # as the "Desktop Background" configuration profile.
 
-New-Item -Path "c:\" -Name "wp" -ItemType "directory" -Force
-Invoke-WebRequest -Uri "https://www.leal.com.au/wallpapers/happyhaven/background.jpg" -OutFile C:\wp\background.jpg
-Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\wp\background.jpg"
-rundll32.exe user32.dll, UpdatePerUserSystemParameters 1, True
+$RegKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$DesktopPath = "DesktopImagePath"
+$DesktopStatus = "DesktopImageStatus"
+$DesktopUrl = "DesktopImageUrl"
+$StatusValue = "1"
+$url = "https://<our url>/wallpapers/<client>/background.jpg"
+$DesktopImageValue = "C:\MEM\wallpaper.jpg"
+$directory = "C:\MEM\"
+
+If ((Test-Path -Path $directory) -eq $false)
+{
+    New-Item -Path $directory -ItemType directory -Force
+}
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $DesktopImageValue)
+
+if (!(Test-Path $RegKeyPath))
+{
+    Write-Host "Creating registry path $($RegKeyPath)."
+    New-Item -Path $RegKeyPath -Force | Out-Null
+}
+
+New-ItemProperty -Path $RegKeyPath -Name $DesktopStatus -Value $Statusvalue -PropertyType DWORD -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopPath -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
+New-ItemProperty -Path $RegKeyPath -Name $DesktopUrl -Value $DesktopImageValue -PropertyType STRING -Force | Out-Null
