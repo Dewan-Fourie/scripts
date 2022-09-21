9fa7bdeec0a4d80733f882d61b7d87f31f8073ad
Dewan-Fourie <dewan@tiamat.com.au>
2022-09-21 11:02:42 +0930

    Completed and Tested

--- Change SharePoint Permissions.ps1
+++ Change SharePoint Permissions.ps1
@@ -1,14 +1,19 @@
-$SiteURL = "https://contoso.sharepoint.com/sites/<site>"
+# Get location of PnP SharePoint module using "Get-Module -ListAvailable" command in powershell.
+Import-Module '<SharePoint PnP Module>'
+
+$SiteURL = '<Site URL>'
+$ListName = '<List Name>'
  
 Try {
     #Connect to PnP Online
-    Connect-PnPOnline -Url $SiteURL -Interactive
+    Connect-PnPOnline -Url $SiteURL -UseWebLogin
  
     #Get the Default Members Group 
     $MembersGroup = Get-PnPGroup -AssociatedMemberGroup
+    Write-Host $($MembersGroup.Title) -ForegroundColor Blue
    
     #Grant "Contribute" permission to the Group and remove "Edit"
-    Set-PnPGroupPermissions -Identity $MembersGroup -AddRole "Contribute" -RemoveRole "Edit"
+    Set-PnPGroupPermissions -Identity $($MembersGroup.Title) -List $ListName -AddRole "Read" -RemoveRole "Edit"
  
     Write-Host "Removed 'Edit' Permissions and Granted 'Contribute' to $($MembersGroup.Title). " -foregroundcolor Green
 }
