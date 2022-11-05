b7e335d362b7f8e73280e0f582727763f24d15cc
Dewan-Fourie <dewan@tiamat.com.au>
2022-11-05 11:02:09 +0930

    Tested and completed

--- Delete 365 User Immidiately.ps1
+++ Delete 365 User Immidiately.ps1
@@ -1,4 +1,8 @@
 Import-Module "C:\Users\Work\OneDrive - Leal Technology\Documents\PowerShell\Modules\ExchangeOnlineManagement\2.0.5\ExchangeOnlineManagement.psd1"
-Connect-ExchangeOnline -UserPrincipalName 'admin@tecautomation.onmicrosoft.com'
-Remove-Mailbox -Identity "ryan.smith@tecautomation.com.au" -Permanent
+Connect-ExchangeOnline -UserPrincipalName '<Global Admin>'
+Remove-Mailbox -Identity "<Email Address to Remove>" -Permanent
 Disconnect-ExchangeOnline
+
+# List active users.
+# $dbs = Get-MailboxDatabase
+# $dbs | foreach {Get-MailboxStatistics -Database $_.DistinguishedName} | where {$_.DisplayName -eq "<DisplayName>"}
---
+++ Delete All Site Collections.ps1
@@ -0,0 +1,17 @@
+import-module "C:\Program Files\PowerShell\Modules\Microsoft.Online.SharePoint.PowerShell"
+
+$AdminSiteURL="https://<Tenancy>-admin.sharepoint.com"
+$OneDriveSiteUrl="https://<Tenancy>-my.sharepoint.com/personal/<Personal Site>"
+ 
+#Get Credentials to connect to SharePoint Admin Center
+$Cred = Get-Credential
+ 
+#Connect to SharePoint Online Admin Center
+Connect-SPOService -Url $AdminSiteURL -credential $Cred
+ 
+#Remove OneDrive Site Collection
+Remove-SPOSite -identity $OneDriveSiteUrl
+Write-Host "OneDrive site collection deleted successfully"
+
+
+#Read more: https://www.sharepointdiary.com/2017/04/how-to-delete-onedrive-for-business-site-collection-sharepoint-online.html#ixzz7jYV3riag
