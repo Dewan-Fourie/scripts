09b6f27525c7a99fc1f3074b1c7f0820e0ae1603
Dewan-Fourie <dewan@tiamat.com.au>
2022-11-29 00:43:35 +0930

    Tested and completed

--- Get Shared Mailbox Members.ps1
+++ Get Shared Mailbox Members.ps1
@@ -1,11 +1,20 @@
 # Use (Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.5) in powershell to install nessesary EXO V2 modules
 # https://www.michev.info/Blog/Post/2997/connecting-to-exchange-online-powershell-via-client-secret-flow <- Used as reference
 
-Import-Module "<Path of EXO V2 Modules>"
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\ExchangeOnlineManagement\2.0.5\ExchangeOnlineManagement.psd1"
 
-Connect-ExchangeOnline -UserPrincipalName '<Tenant Admin>'
+Connect-ExchangeOnline -UserPrincipalName 'nfit@amsant.onmicrosoft.com'
+
+$InputFilePath = "C:\Users\Work\OneDrive - Leal Technology\Documents\AMSANT\sharedmailboxes.csv" #Set this to the CSV file of the accounts you want to provision.
+# The csv file needs 'Mailbox' as the header, with all the accounts listed under it.
+
+$CsvFile = Import-Csv $InputFilePath
 
 # Enter mailbox to check below
-Get-MailboxPermission -Identity '<Shared Mailbox>' | Format-List # Select-Object Name,Alias,UserPrincipalName,PrimarySmtpAddress,@{l='SendOnBehalfOf';e={$_. -join " ;"}} | Export-CSV "C:\temp\SendOnBehalf.csv"
+ForEach ($line in $CsvFile)
+{ 
+    $filepath = 'C:\temp\Mailboxes\' + $line.Mailbox + '.csv'
+    Get-MailboxPermission -Identity $line.Mailbox | Select-Object User | Export-CSV $filepath
+}
 
 Disconnect-ExchangeOnline
