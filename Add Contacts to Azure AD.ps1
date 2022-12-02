56e528bac8c5b567bb7f41aa3d03311b48aed3f0
Dewan-Fourie <dewan@tiamat.com.au>
2022-12-02 11:54:05 +0930

    Changed Output

--- Add Contacts to Azure AD.ps1
+++ Add Contacts to Azure AD.ps1
@@ -1,13 +1,13 @@
 # Import dependancies
-Import-Module "<EXO V2 Module>"
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\ExchangeOnlineManagement"
 
-Connect-ExchangeOnline -UserPrincipalName '<Tenant Global/Exchange Administrator>'
+Connect-ExchangeOnline -UserPrincipalName 'nfit@amsant.onmicrosoft.com'
 
-$UsersCSV = Import-Csv "<Path of CSV>"
+$UsersCSV = Import-Csv "C:\Users\Work\OneDrive - Leal Technology\Documents\AMSANT\External Users\test.csv"
 # CSV must have Email and Username column titles
 
 foreach ($line in $UsersCSV){
-    # New-MailContact -Name $line.Username -ExternalEmailAddress $line.Email
+    New-MailContact -Name $line.Username -ExternalEmailAddress $line.Email | Out-Null
     if ($null -ne (Get-Contact -Identity $line.Email -Erroraction Silentlycontinue)){
         Write-Host $line.Username was added successfully -ForegroundColor Green
     } else {
