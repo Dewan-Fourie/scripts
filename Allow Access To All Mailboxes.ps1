ce476f4d4cab28320164adfbfb6a6276af2a085d
Dewan-Fourie <dewan@tiamat.com.au>
2022-12-09 14:44:31 +0930

    Refactored to make use of default permissions

--- Allow Access To All Mailboxes.ps1
+++ Allow Access To All Mailboxes.ps1
@@ -1,28 +1,14 @@
-#GIVE ALL USERs ACCESS TO ALL CALENDAR
+# Script to give user permission to all calendars in tenancy.
+Import-Module "C:\Program Files\WindowsPowerShell\Modules\ExchangeOnlineManagement"
+Connect-ExchangeOnline -UserPrincipalName "<Global/Exchange Admin>"
 
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session
-Write-Output "======================================================="
-Write-host "============= Setting Calendar Permission =============" -foreground "yellow"
-Write-Output "======================================================="
-$useAccess = Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "UserMailbox"} | Select Identity
-Clear-Host
-Write-Host "AccessRights: None, Owner, PublishingEditor, Editor, PublishingAuthor, Author, NonEditingAuthor, Reviewer, Contributor" -foreground "yellow"
-Write-Host "This script will attempt to modify and add a user permission. You may have an error message if the user is already added. Please ignore." -foreground "darkcyan"
-Write-Host "---------------------" -foreground "gray"
-$accRight=read-host "Please enter the desired access right for all users. Ex: Reviewer"
-ForEach ( $user in $useAccess ) {
-Get-Mailbox -ResultSize Unlimited | ForEach {
-If ( $_.Identity -ne $user.Identity ) {
-Add-MailboxFolderPermission "$($_.SamAccountName):\calendar" -User $user.Identity -AccessRights $accRight
-Set-MailboxFolderPermission "$($_.SamAccountName):\calendar" -User $user.Identity -AccessRights $accRight
+# Options: Owner, Publishing Editor, Editor, Publishing Author, Author, Nonediting Author, Reviewer, Contributor, None
+$accRight = "Reviewer"
+
+# Collects a list of all users, and adds these user's permissions to the list created previously ($userAccess)
+Get-Mailbox -ResultSize Unlimited | ForEach-Object {
+    Set-MailboxFolderPermission "$($_.SamAccountName):\calendar" -User "Default" -AccessRights $accRight
 }
-}
-}
-Clear-Host
-Write-Output "======================================================="
-Write-host "============ CALENDAR INFORMATION UPDATED =============" -foreground "green"
-Write-Output "======================================================="
-ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select Identity,User,AccessRights | ft -Wrap -AutoSize}
+
+# Output to check changes
+ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select-Object Identity,User,AccessRights | Format-Table -Wrap -AutoSize}
