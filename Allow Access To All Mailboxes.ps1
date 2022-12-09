6286979587e975ae55509fcbd26ba7c82428c204
Dewan-Fourie <dewan@tiamat.com.au>
2022-12-09 14:59:16 +0930

    Refactored to make use of default permissions

--- Allow Access To All Mailboxes.ps1
+++ Allow Access To All Mailboxes.ps1
@@ -1,5 +1,5 @@
 # Script to give user permission to all calendars in tenancy.
-Import-Module "C:\Program Files\WindowsPowerShell\Modules\ExchangeOnlineManagement"
+Import-Module "<Path of EXO V2>"
 Connect-ExchangeOnline -UserPrincipalName "<Global/Exchange Admin>"
 
 # Options: Owner, Publishing Editor, Editor, Publishing Author, Author, Nonediting Author, Reviewer, Contributor, None
@@ -8,7 +8,8 @@ $accRight = "Reviewer"
 # Collects a list of all users, and adds these user's permissions to the list created previously ($userAccess)
 Get-Mailbox -ResultSize Unlimited | ForEach-Object {
     Set-MailboxFolderPermission "$($_.SamAccountName):\calendar" -User "Default" -AccessRights $accRight
+    Write-Host "Default permission changed for" $_.SamAccountName "to" $accRight -ForegroundColor Green
 }
 
 # Output to check changes
-ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select-Object Identity,User,AccessRights | Format-Table -Wrap -AutoSize}
+# ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select-Object Identity,User,AccessRights | Format-Table -Wrap -AutoSize}
