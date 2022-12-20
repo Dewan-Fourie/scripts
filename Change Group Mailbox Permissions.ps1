b098957cb19603dcb90df94d3e02af6364caaa98
Dewan-Fourie <dewan@tiamat.com.au>
2022-12-20 11:33:46 +0930

    Completed and tested

--- Change Group Mailbox Permissions.ps1
+++ Change Group Mailbox Permissions.ps1
@@ -2,14 +2,10 @@
 Import-Module "<Path of EXO V2>"
 Connect-ExchangeOnline -UserPrincipalName "<Global/Exchange Admin>"
 
-# Options: Owner, Publishing Editor, Editor, Publishing Author, Author, Nonediting Author, Reviewer, Contributor, None
-$accRight = "Reviewer"
+Set-UnifiedGroup "<Group Mailbox>" -CalendarMemberReadOnly:$true
+Get-MailboxFolderPermission -Identity "<Group Mailbox>:\Calendar" -GroupMailbox
 
-# Collects a list of all users, and adds these user's permissions to the list created previously ($userAccess)
-Get-Mailbox -ResultSize Unlimited | ForEach-Object {
-    Set-MailboxFolderPermission "$($_.SamAccountName):\calendar" -User "Default" -AccessRights $accRight
-    Write-Host "Default permission changed for" $_.SamAccountName "to" $accRight -ForegroundColor Green
-}
+Disconnect-ExchangeOnline
 
 # Output to check changes
 # ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select-Object Identity,User,AccessRights | Format-Table -Wrap -AutoSize}
