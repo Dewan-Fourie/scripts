a1220112571aca59030c181ec831bf6a7753779b
Dewan-Fourie <dewan@tiamat.com.au>
2022-12-23 13:52:02 +0930

    Added and tested.

--- Change Group Mailbox Permissions.ps1
+++ Change Group Mailbox Permissions.ps1
@@ -1,11 +1,18 @@
 # Script to give user permission to all calendars in tenancy.
-Import-Module "<Path of EXO V2>"
-Connect-ExchangeOnline -UserPrincipalName "<Global/Exchange Admin>"
+Import-Module "<Full path of EXO V2 module>"
+Connect-ExchangeOnline -UserPrincipalName "<GLobal/Mailbox Admin>"
 
-Set-UnifiedGroup "<Group Mailbox>" -CalendarMemberReadOnly:$true
-Get-MailboxFolderPermission -Identity "<Group Mailbox>:\Calendar" -GroupMailbox
+# This only gets the Microsoft 365 Groups.
+Get-UnifiedGroup -ResultSize Unlimited | Select-Object PrimarySmtpAddress | ForEach-Object {
+    Set-UnifiedGroup $_.PrimarySmtpAddress -CalendarMemberReadOnly:$true
+    $FolderMailbox = $_.PrimarySmtpAddress + ":\calendar"
+    $validation = Get-MailboxFolderPermission $FolderMailbox -User "Member@local" -GroupMailbox | Select-Object AccessRights
+    # Validation
+    if("Reviewer" -eq $validation.AccessRights){
+        Write-Host $_.PrimarySmtpAddress "'s Member@local member was changed to read only." -ForegroundColor Green
+    } else {
+        Write-Host "Something went wrong with changing " $_.PrimarySmtpAddress "'s Member@local permissions." -ForegroundColor red
+    }
+}
 
 Disconnect-ExchangeOnline
-
-# Output to check changes
-# ForEach ($mbx in Get-Mailbox -ResultSize Unlimited) {Get-MailboxFolderPermission ($mbx.Name + “:\Calendar”) | Select-Object Identity,User,AccessRights | Format-Table -Wrap -AutoSize}
